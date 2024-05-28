12d362641b2c851fe6a0f374241f7a48a78057a7
yhanyi <yeohhanyi0916@gmail.com>
2024-05-28 21:55:21 +0800

    Changed header

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import TearsheetDisplay from "@/components/tearsheet";
-import UserInput from "../../components/testinput";
 import React from "react";
+import TearsheetViewer from "@/components/tearsheet_viewer";
+import BacktestInput from "@/components/backtest_input";
 
 export default function Home() {
   return (
     <main>
       <div>This is the playground page!</div>
       <div>
-        <h1>Test Input</h1>
-        <UserInput />
-        <TearsheetDisplay />
+        <h1>Input the parameters you would like to backtest.</h1>
+        <BacktestInput />
+        <TearsheetViewer />
       </div>
     </main>
   );
