08e7889209edb17f7ac7ac4365d1562b09cd77fe
yhanyi <yeohhanyi0916@gmail.com>
2024-05-30 18:42:26 +0800

    Setup modelzoo with pretrained finbert

---
+++ backend/modelzoo.py
@@ -0,0 +1,20 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Initialize ProsusAI/finbert
+tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert").to(device)
+labels = ["positive", "negative", "neutral"]
+
+def finbert_estimate_sentiment(news):
+    if not news:
+        return 0, labels[-1]
+
+    tokens = tokenizer(news, return_tensors="pt", padding=True).to(device)
+    result = model(tokens["input_ids"], attention_mask=tokens["attention_mask"])["logits"]
+    result = torch.nn.functional.softmax(torch.sum(result, 0), dim=-1)
+    probability = result[torch.argmax(result)]
+    sentiment = labels[torch.argmax(result)]
+    return probability, sentiment
