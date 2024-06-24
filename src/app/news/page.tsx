108686e854a79794c8395206c95b483eaac94043
yhanyi <yeohhanyi0916@gmail.com>
2024-06-24 23:10:18 +0800

    Added News display component

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import News from "@/components/news_displayer";
 
 export default function Home() {
   return (
@@ -13,14 +14,23 @@ export default function Home() {
         animate={{ y: 0, opacity: 1 }}
       >
         <h1 className="text-4xl font-bold">News</h1>
-        <Image src="/images/News.png" alt="News" width={48} height={48}/>
+        <Image src="/images/News.png" alt="News" width={48} height={48} />
       </motion.div>
       <div className="flex flex-col items-center space-y-5">
         <h1 className="text-4xl font-bold">News</h1>
         <div>
-          <img className="mx-auto" src="https://alternative.me/crypto/fear-and-greed-index.png" alt="Latest Crypto Fear & Greed Index" width={250} height="auto"/>
+          <img
+            className="mx-auto"
+            src="https://alternative.me/crypto/fear-and-greed-index.png"
+            alt="Latest Crypto Fear & Greed Index"
+            width={250}
+            height="auto"
+          />
           <div className="h-2"></div>
           <h1 className="text-xl font-bold">Current Fear and Greed Index</h1>
+        </div>
+        <div>
+          <News />
         </div>
       </div>
     </main>
