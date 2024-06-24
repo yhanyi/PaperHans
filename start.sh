c7a8b999bf65827d7515f234d9043470b807d0a2
yhanyi <yeohhanyi0916@gmail.com>
2024-06-24 23:32:07 +0800

    Added startup script

---
+++ start.sh
@@ -0,0 +1,17 @@
+# Start Flask server
+echo "Starting Flask server."
+cd backend
+python3 news_fetcher.py &
+
+# Start Uvicorn server
+echo "Starting Uvicorn server."
+uvicorn playground_io:app --reload &
+
+# Start Next.js frontend.
+cd ..
+echo "Starting Next.js frontend."
+npm run dev
+
+# Wait for all processes to finish.
+wait
+echo "All processes finished, ready for dev testing."
