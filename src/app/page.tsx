59752dd4d1b3cbd9dbc073485a4ffb4481078e1b
yhanyi <yeohhanyi0916@gmail.com>
2024-07-09 00:11:35 +0800

    Moved authbutton to header instead of main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-"use client"
+"use client";
 
-import AuthButton from "../components/AuthButton";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
@@ -9,9 +8,13 @@ export default function Home() {
 
   return (
     <main>
-      <AuthButton />
       <div className="w-full px-8 py-12 md:py-20 flex flex-col items-center">
-        <Image src="/images/PaperHans.png" alt="PaperHans Logo" width={72} height={72}/>
+        <Image
+          src="/images/PaperHans.png"
+          alt="PaperHans Logo"
+          width={72}
+          height={72}
+        />
         <h1 className="text-center text-4xl md:text-6xl max-w-xl font-semibold">
           PaperHans
         </h1>
@@ -19,7 +22,9 @@ export default function Home() {
           Your new one stop crypto webapp!
         </p>
         <button
-          onClick={() => {router.push('/aboutUs');}}
+          onClick={() => {
+            router.push("/aboutUs");
+          }}
           className="bg-indigo-600 text-white text-base md:text-lg font-medium px-8 py-2 shadow-[3px_3px_0_black] hover:shadow-[1px_1px_0_black] hover:translate-x-[3px] hover:translate-y-[3px] transition-all"
         >
           About Us
