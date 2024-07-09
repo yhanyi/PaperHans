f5c19169a6260d9f1f5087c6e36e484d88489341
yhanyi <yeohhanyi0916@gmail.com>
2024-07-09 09:03:28 +0800

    Created links const in lib

---
+++ src/lib/links-data.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "/",
+  },
+  {
+    name: "Prices",
+    hash: "/prices",
+  },
+  {
+    name: "Playground",
+    hash: "/playground",
+  },
+  {
+    name: "News",
+    hash: "/news",
+  },
+  {
+    name: "Learn",
+    hash: "/learn",
+  },
+];
