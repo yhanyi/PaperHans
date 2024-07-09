dc462429c3071079959ca837cc9b23deee057bd3
yhanyi <yeohhanyi0916@gmail.com>
2024-07-09 18:29:23 +0800

    Improved mobile responsiveness for Learn page

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -7,16 +7,26 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <motion.div
+      {/* <motion.div
         className="fixed top-8 right-8 flex items-center"
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
       >
         <h1 className="text-4xl font-bold">Learn</h1>
         <Image src="/images/Learn.png" alt="Learn" width={48} height={48}/>
-      </motion.div>
+      </motion.div> */}
       <div className="flex flex-col items-center space-y-5">
-        <h1 className="text-4xl font-bold">Learn</h1>
+        <motion.div
+          className="flex items-center gap-2"
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <h1 className="text-2xl md:text-4xl font-bold">Learn</h1>
+          <Image src="/images/Learn.png" alt="Learn" width={48} height={48} />
+        </motion.div>
+        <div className="flex flex-col items-center space-y-5">
+          <h1 className="text-4xl font-bold">Learn</h1>
+        </div>
       </div>
     </main>
   );
