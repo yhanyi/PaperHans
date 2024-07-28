6e5a8ac709dec5cf5a1b240388592a21cf21a452
yhanyi <yeohhanyi0916@gmail.com>
2024-07-28 10:24:08 +0800

    Created Procfile for potential Heroku deployment

--- api/main.py
+++ api/main.py
@@ -1,115 +1,118 @@
-# from fastapi import FastAPI, HTTPException
-# from fastapi.responses import JSONResponse, FileResponse
-# from fastapi.middleware.cors import CORSMiddleware
-# from pydantic import BaseModel
-# import requests
-# from dotenv import load_dotenv
-# import os
-# import datetime
-# import trading_script as trade
-# import uvicorn
-# from modelzoo import analyze_sentiment
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import requests
+from dotenv import load_dotenv
+import os
+import datetime
+import trading_script as trade
+import uvicorn
+from modelzoo import analyze_sentiment
 
-# load_dotenv()
+load_dotenv()
 
-# app = FastAPI()
+app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 
-# def fetch_crypto_news(query='cryptocurrency'):
-#     today = datetime.date.today()
-#     startdate = today - datetime.timedelta(days=30)
-#     url = f'https://newsapi.org/v2/everything?q={query}&from={startdate.strftime("%Y-%m-%d")}&sortBy=publishedAt&apiKey={NEWS_API_KEY}&language=en'
-#     response = requests.get(url)
-#     if response.status_code == 429: 
-#         return None, response.status_code
-#     data = response.json()
-#     return data['articles'], response.status_code
+def fetch_crypto_news(query='cryptocurrency'):
+    today = datetime.date.today()
+    startdate = today - datetime.timedelta(days=30)
+    url = f'https://newsapi.org/v2/everything?q={query}&from={startdate.strftime("%Y-%m-%d")}&sortBy=publishedAt&apiKey={NEWS_API_KEY}&language=en'
+    response = requests.get(url)
+    if response.status_code == 429: 
+        return None, response.status_code
+    data = response.json()
+    return data['articles'], response.status_code
 
-# def process_news(news):
-#     processed_news = []
-#     for item in news:
-#         processed_news.append({
-#             "title": item.get("title", "No Title"),
-#             "info": item.get("description", "No Description"),
-#             "url": item.get("url", "No URL")
-#         })
-#     return processed_news
+def process_news(news):
+    processed_news = []
+    for item in news:
+        processed_news.append({
+            "title": item.get("title", "No Title"),
+            "info": item.get("description", "No Description"),
+            "url": item.get("url", "No URL")
+        })
+    return processed_news
 
-# @app.get("/api/news")
-# def get_news():
-#     news, status_code = fetch_crypto_news()
-#     if status_code == 429:
-#         return JSONResponse(content={"error": "Rate limit exceeded. Please try again later."}, status_code=429)
-#     processed_news = process_news(news)
-#     news_with_sentiment = analyze_sentiment(processed_news)
-#     return JSONResponse(content=news_with_sentiment)
+@app.get("/api/news")
+def get_news():
+    news, status_code = fetch_crypto_news()
+    print("Reached 1")
+    if status_code == 429:
+        return JSONResponse(content={"error": "Rate limit exceeded. Please try again later."}, status_code=429)
+    processed_news = process_news(news)
+    print("Reached 2")
+    news_with_sentiment = analyze_sentiment(processed_news)
+    print("Reached 3")
+    return JSONResponse(content=news_with_sentiment)
 
-# class BacktestParameters(BaseModel):
-#     symbol: str
-#     year: str
-#     benchmark: str
-#     cashAtRisk: str
-#     userId: str
+class BacktestParameters(BaseModel):
+    symbol: str
+    year: str
+    benchmark: str
+    cashAtRisk: str
+    userId: str
 
-# # Caches backtesting status
-# backtest_status = {"status": "idle"}
+# Caches backtesting status
+backtest_status = {"status": "idle"}
 
-# @app.post("/api/process")
-# async def process_data(bp: BacktestParameters):
-#     try:
-#         backtest_status["status"] = "running"
-#         result = await trade.backtestStrategy(bp.symbol, int(bp.year), bp.benchmark, float(bp.cashAtRisk), bp.userId)
-#         backtest_status["status"] = "complete"
-#         cleanup_logs_files()
-#         return {"result": result}
-#     except Exception as e:
-#         backtest_status["status"] = "error"
-#         print(f"Error in process_data: {str(e)}")
-#         raise HTTPException(status_code=500, detail=str(e))
+@app.post("/api/process")
+async def process_data(bp: BacktestParameters):
+    try:
+        backtest_status["status"] = "running"
+        result = await trade.backtestStrategy(bp.symbol, int(bp.year), bp.benchmark, float(bp.cashAtRisk), bp.userId)
+        backtest_status["status"] = "complete"
+        cleanup_logs_files()
+        return {"result": result}
+    except Exception as e:
+        backtest_status["status"] = "error"
+        print(f"Error in process_data: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
 
-# @app.get("/api/status")
-# async def get_status():
-#     return backtest_status
+@app.get("/api/status")
+async def get_status():
+    return backtest_status
 
-# LOGS_DIRECTORY = os.path.join(os.path.dirname(__file__), 'logs')
+LOGS_DIRECTORY = os.path.join(os.path.dirname(__file__), 'logs')
 
-# def cleanup_logs_files():
-#     trades_fp = os.path.join(LOGS_DIRECTORY, 'trades.html')
-#     tearsheet_fp = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
-#     if os.path.isfile(trades_fp):
-#         os.remove(trades_fp)
-#     if os.path.isfile(tearsheet_fp):
-#         os.remove(tearsheet_fp)
-#     files = os.listdir(LOGS_DIRECTORY)
-#     for file in files:
-#         file_path = os.path.join(LOGS_DIRECTORY, file)
-#         if not file.endswith(".html"):
-#             os.remove(file_path)
-#         elif "trades" in file:
-#             new_file_path = os.path.join(LOGS_DIRECTORY, 'trades.html')
-#             os.rename(file_path, new_file_path)
-#         elif "tearsheet" in file:
-#             new_file_path = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
-#             os.rename(file_path, new_file_path)
+def cleanup_logs_files():
+    trades_fp = os.path.join(LOGS_DIRECTORY, 'trades.html')
+    tearsheet_fp = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
+    if os.path.isfile(trades_fp):
+        os.remove(trades_fp)
+    if os.path.isfile(tearsheet_fp):
+        os.remove(tearsheet_fp)
+    files = os.listdir(LOGS_DIRECTORY)
+    for file in files:
+        file_path = os.path.join(LOGS_DIRECTORY, file)
+        if not file.endswith(".html"):
+            os.remove(file_path)
+        elif "trades" in file:
+            new_file_path = os.path.join(LOGS_DIRECTORY, 'trades.html')
+            os.rename(file_path, new_file_path)
+        elif "tearsheet" in file:
+            new_file_path = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
+            os.rename(file_path, new_file_path)
         
-# @app.get("/api/tearsheet")
-# async def get_tearsheet():
-#     try:
-#         file_path = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
-#         if not os.path.isfile(file_path):
-#             raise HTTPException(status_code=404, detail="Tearsheet not found")
-#         return FileResponse(file_path, media_type='text/html', filename='tearsheet.html')
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+@app.get("/api/tearsheet")
+async def get_tearsheet():
+    try:
+        file_path = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
+        if not os.path.isfile(file_path):
+            raise HTTPException(status_code=404, detail="Tearsheet not found")
+        return FileResponse(file_path, media_type='text/html', filename='tearsheet.html')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-# # if __name__ == '__main__':
-# #     uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
--- api/trading_script.py
+++ api/trading_script.py
@@ -1,142 +1,142 @@
-# from lumibot.brokers import Alpaca
-# from lumibot.backtesting import YahooDataBacktesting
-# from lumibot.strategies.strategy import Strategy
-# # from lumibot.traders import Trader
-# # from lumibot.entities import Asset
-# from datetime import datetime, timedelta
-# from modelzoo import finbert_estimate_sentiment
-# from dotenv import load_dotenv
-# from alpaca_trade_api.rest import REST
-# from os import getenv
-# import json
-# from firebase_admin import credentials, initialize_app, firestore
-# from dotenv import load_dotenv
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+# from lumibot.traders import Trader
+# from lumibot.entities import Asset
+from datetime import datetime, timedelta
+from modelzoo import finbert_estimate_sentiment
+from dotenv import load_dotenv
+from alpaca_trade_api.rest import REST
+from os import getenv
+import json
+from firebase_admin import credentials, initialize_app, firestore
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-# firebase_config = json.loads(getenv("FIREBASE_JSON"))
-# cred = credentials.Certificate(firebase_config)
-# initialize_app(cred)
-# db = firestore.client()
+firebase_config = json.loads(getenv("FIREBASE_JSON"))
+cred = credentials.Certificate(firebase_config)
+initialize_app(cred)
+db = firestore.client()
 
-# def get_alpaca_keys(uid):
-#     try:
-#         doc_ref = db.collection("alpacaKeys").document(uid)
-#         doc = doc_ref.get()
-#         if doc.exists:
-#             data = doc.to_dict()
-#             if data and "apiKey" in data and "apiSecret" in data:
-#                 return data["apiKey"], data["apiSecret"]
-#             else:
-#                 raise ValueError("Alpaca API keys are incomplete for this user")
-#         else:
-#             raise ValueError("No Alpaca API keys found for user")
+def get_alpaca_keys(uid):
+    try:
+        doc_ref = db.collection("alpacaKeys").document(uid)
+        doc = doc_ref.get()
+        if doc.exists:
+            data = doc.to_dict()
+            if data and "apiKey" in data and "apiSecret" in data:
+                return data["apiKey"], data["apiSecret"]
+            else:
+                raise ValueError("Alpaca API keys are incomplete for this user")
+        else:
+            raise ValueError("No Alpaca API keys found for user")
     
-#     except Exception as e:
-#         raise
+    except Exception as e:
+        raise
 
-# class MLTrader(Strategy):
-#     def initialize(self, symbol, cash_at_risk=0.5, alpaca_api_key=None, alpaca_api_secret=None):
-#         self.symbol = symbol
-#         self.sleeptime = "24H"
-#         self.last_trade = None
-#         self.cash_at_risk = cash_at_risk
-#         self.api = REST(base_url="https://paper-api.alpaca.markets",
-#                         key_id=alpaca_api_key,
-#                         secret_key=alpaca_api_secret)
+class MLTrader(Strategy):
+    def initialize(self, symbol, cash_at_risk=0.5, alpaca_api_key=None, alpaca_api_secret=None):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+        self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url="https://paper-api.alpaca.markets",
+                        key_id=alpaca_api_key,
+                        secret_key=alpaca_api_secret)
 
-#     def position_sizing(self):
-#         cash = self.get_cash()
-#         last_price = self.get_last_price(self.symbol)
-#         quantity = int(cash * self.cash_at_risk / last_price)
-#         return cash, last_price, quantity
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = int(cash * self.cash_at_risk / last_price)
+        return cash, last_price, quantity
     
-#     def get_dates(self):
-#         today = self.get_datetime()
-#         three_days = today - timedelta(days=3)
-#         return today.strftime("%Y-%m-%d"), three_days.strftime("%Y-%m-%d")
+    def get_dates(self):
+        today = self.get_datetime()
+        three_days = today - timedelta(days=3)
+        return today.strftime("%Y-%m-%d"), three_days.strftime("%Y-%m-%d")
 
-#     def get_sentiment(self):
-#         today, three_days = self.get_dates()
-#         news = self.api.get_news(symbol=self.symbol,
-#                                  start=three_days,
-#                                  end=today)
-#         news = [event.__dict__["_raw"]["headline"] for event in news]
-#         probability, sentiment = finbert_estimate_sentiment(news)
-#         return probability, sentiment
+    def get_sentiment(self):
+        today, three_days = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol,
+                                 start=three_days,
+                                 end=today)
+        news = [event.__dict__["_raw"]["headline"] for event in news]
+        probability, sentiment = finbert_estimate_sentiment(news)
+        return probability, sentiment
 
-#     def on_trading_iteration(self):
-#         cash, last_price, quantity = self.position_sizing()
-#         probability, sentiment = self.get_sentiment()
-#         if cash > last_price:
-#             if sentiment == "positive" and probability > 0.999:
-#                 if self.last_trade == "sell":
-#                     self.sell_all()
-#                 order = self.create_order(
-#                     self.symbol,
-#                     quantity,
-#                     "buy",
-#                     type="bracket",
-#                     take_profit_price=last_price*1.2,
-#                     stop_loss_price=last_price*0.95
-#                 )
-#                 self.submit_order(order)
-#                 self.last_trade = "buy"
+    def on_trading_iteration(self):
+        cash, last_price, quantity = self.position_sizing()
+        probability, sentiment = self.get_sentiment()
+        if cash > last_price:
+            if sentiment == "positive" and probability > 0.999:
+                if self.last_trade == "sell":
+                    self.sell_all()
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type="bracket",
+                    take_profit_price=last_price*1.2,
+                    stop_loss_price=last_price*0.95
+                )
+                self.submit_order(order)
+                self.last_trade = "buy"
 
-#             elif sentiment == "negative" and probability > 0.999:
-#                 if self.last_trade == "buy":
-#                     self.sell_all()
-#                 order = self.create_order(
-#                     self.symbol,
-#                     quantity,
-#                     "sell",
-#                     type="bracket",
-#                     take_profit_price=last_price*0.8,
-#                     stop_loss_price=last_price*1.05
-#                 )
-#                 self.submit_order(order)
-#                 self.last_trade = "sell"
+            elif sentiment == "negative" and probability > 0.999:
+                if self.last_trade == "buy":
+                    self.sell_all()
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "sell",
+                    type="bracket",
+                    take_profit_price=last_price*0.8,
+                    stop_loss_price=last_price*1.05
+                )
+                self.submit_order(order)
+                self.last_trade = "sell"
 
-# def init_broker(alpaca_api_key, alpaca_api_secret):
-#     ALPACA_CREDS = {
-#       "API_KEY": alpaca_api_key,
-#       "API_SECRET": alpaca_api_secret,
-#       "PAPER": True
-#     }
-#     broker = Alpaca(ALPACA_CREDS)
-#     return broker
+def init_broker(alpaca_api_key, alpaca_api_secret):
+    ALPACA_CREDS = {
+      "API_KEY": alpaca_api_key,
+      "API_SECRET": alpaca_api_secret,
+      "PAPER": True
+    }
+    broker = Alpaca(ALPACA_CREDS)
+    return broker
 
-# async def backtestStrategy(symbol, year, benchmark, cash_at_risk, user_id):
-#   try:
-#     alpaca_api_key, alpaca_api_secret = get_alpaca_keys(user_id)
-#     broker = init_broker(alpaca_api_key, alpaca_api_secret)
-#     strategy = MLTrader(name="mlstrategy",
-#                         broker=broker,
-#                         parameters={
-#                             "symbol": symbol.upper(),
-#                             "cash_at_risk": cash_at_risk,
-#                             "alpaca_api_key": alpaca_api_key,
-#                             "alpaca_api_secret": alpaca_api_secret
-#                         })
+async def backtestStrategy(symbol, year, benchmark, cash_at_risk, user_id):
+  try:
+    alpaca_api_key, alpaca_api_secret = get_alpaca_keys(user_id)
+    broker = init_broker(alpaca_api_key, alpaca_api_secret)
+    strategy = MLTrader(name="mlstrategy",
+                        broker=broker,
+                        parameters={
+                            "symbol": symbol.upper(),
+                            "cash_at_risk": cash_at_risk,
+                            "alpaca_api_key": alpaca_api_key,
+                            "alpaca_api_secret": alpaca_api_secret
+                        })
   
 
-#     start_date = datetime(year, 1, 1)
-#     end_date = datetime(year, 12, 31)
-#     strategy.backtest(
-#         YahooDataBacktesting,
-#         start_date,
-#         end_date,
-#         benchmark_asset=benchmark.upper(),
-#         parameters={
-#             "symbol": symbol.upper(),
-#             "cash_at_risk": cash_at_risk,
-#             "alpaca_api_key": alpaca_api_key,
-#             "alpaca_api_secret": alpaca_api_secret
-#         },
-#         show_tearsheet=False,
-#         show_plot=False
-#     )
-#     state = "Completed"
-#   except:
-#      state = "Error"
-#   return state
+    start_date = datetime(year, 1, 1)
+    end_date = datetime(year, 12, 31)
+    strategy.backtest(
+        YahooDataBacktesting,
+        start_date,
+        end_date,
+        benchmark_asset=benchmark.upper(),
+        parameters={
+            "symbol": symbol.upper(),
+            "cash_at_risk": cash_at_risk,
+            "alpaca_api_key": alpaca_api_key,
+            "alpaca_api_secret": alpaca_api_secret
+        },
+        show_tearsheet=False,
+        show_plot=False
+    )
+    state = "Completed"
+  except:
+     state = "Error"
+  return state
