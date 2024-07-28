414770fa4a30eee45d82c51a19f700361770aa5b
yhanyi <yeohhanyi0916@gmail.com>
2024-07-28 16:32:54 +0800

    Reduced dependencies

--- api/main.py
+++ api/main.py
@@ -6,8 +6,19 @@ import requests
 from dotenv import load_dotenv
 import os
 import datetime
-import trading_script as trade
-from modelzoo import analyze_sentiment
+import uvicorn
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from datetime import datetime, timedelta
+import json
+from firebase_admin import credentials, initialize_app, firestore
+
+load_dotenv()
+
+"""
+FastAPI Setup
+"""
 
 load_dotenv()
 
@@ -21,11 +32,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+"""
+CONSTANTS
+"""
 
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+LOGS_DIRECTORY = os.path.join(os.path.dirname(__file__), '../logs')
+backtest_status = {"status": "idle"}
+firebase_config = json.loads(os.getenv("FIREBASE_JSON"))
+cred = credentials.Certificate(firebase_config)
+initialize_app(cred)
+db = firestore.client()
+
+"""
+TYPINGS
+"""
+class BacktestParameters(BaseModel):
+    symbol: str
+    year: str
+    benchmark: str
+    cashAtRisk: str
+    userId: str
+
+"""
+UTIL FUNCTIONS
+"""
 def fetch_crypto_news(query='cryptocurrency'):
-    today = datetime.date.today()
-    startdate = today - datetime.timedelta(days=30)
+    today = datetime.now()
+    startdate = today - timedelta(days=30)
     url = f'https://newsapi.org/v2/everything?q={query}&from={startdate.strftime("%Y-%m-%d")}&sortBy=publishedAt&apiKey={NEWS_API_KEY}&language=en'
     response = requests.get(url)
     if response.status_code == 429: 
@@ -43,49 +77,9 @@ def process_news(news):
         })
     return processed_news
 
-@app.get("/api/news")
-def get_news():
-    news, status_code = fetch_crypto_news()
-    print("Reached 1")
-    if status_code == 429:
-        return JSONResponse(content={"error": "Rate limit exceeded. Please try again later."}, status_code=429)
-    processed_news = process_news(news)
-    print("Reached 2")
-    news_with_sentiment = analyze_sentiment(processed_news)
-    print("Reached 3")
-    return JSONResponse(content=news_with_sentiment)
-
-class BacktestParameters(BaseModel):
-    symbol: str
-    year: str
-    benchmark: str
-    cashAtRisk: str
-    userId: str
-
-# Caches backtesting status
-backtest_status = {"status": "idle"}
-
-@app.post("/api/process")
-async def process_data(bp: BacktestParameters):
-    try:
-        backtest_status["status"] = "running"
-        result = await trade.backtestStrategy(bp.symbol, int(bp.year), bp.benchmark, float(bp.cashAtRisk), bp.userId)
-        backtest_status["status"] = "complete"
-        cleanup_logs_files()
-        return {"result": result}
-    except Exception as e:
-        backtest_status["status"] = "error"
-        print(f"Error in process_data: {str(e)}")
-        raise HTTPException(status_code=500, detail=str(e))
-
-@app.get("/api/status")
-async def get_status():
-    return backtest_status
-
-LOGS_DIRECTORY = os.path.join(os.path.dirname(__file__), 'logs')
-
 def cleanup_logs_files():
     trades_fp = os.path.join(LOGS_DIRECTORY, 'trades.html')
+    os.path.join(os.path.dirname(__file__), 'logs')
     tearsheet_fp = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
     if os.path.isfile(trades_fp):
         os.remove(trades_fp)
@@ -102,13 +96,125 @@ def cleanup_logs_files():
         elif "tearsheet" in file:
             new_file_path = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
             os.rename(file_path, new_file_path)
-        
+
+def get_alpaca_keys(uid):
+    try:
+        doc_ref = db.collection("alpacaKeys").document(uid)
+        doc = doc_ref.get()
+        if doc.exists:
+            data = doc.to_dict()
+            if data and "apiKey" in data and "apiSecret" in data:
+                return data["apiKey"], data["apiSecret"]
+            else:
+                raise ValueError("Alpaca API keys are incomplete for this user")
+        else:
+            raise ValueError("No Alpaca API keys found for user")
+    
+    except Exception as e:
+        raise
+class MLTrader(Strategy):
+    def initialize(self, symbol, cash_at_risk=0.5):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+        self.cash_at_risk = cash_at_risk
+
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+        quantity = int(cash * self.cash_at_risk / last_price)
+        return cash, last_price, quantity
+
+    def on_trading_iteration(self):
+        cash, last_price, quantity = self.position_sizing()
+        if cash > last_price:
+            if self.last_trade == None:
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type="bracket",
+                    take_profit_price=last_price*1.2,
+                    stop_loss_price=last_price*0.95
+                )
+                self.submit_order(order)
+                self.last_trade = "buy"
+
+def init_broker(alpaca_api_key, alpaca_api_secret):
+    ALPACA_CREDS = {
+      "API_KEY": alpaca_api_key,
+      "API_SECRET": alpaca_api_secret,
+      "PAPER": True
+    }
+    broker = Alpaca(ALPACA_CREDS)
+    return broker
+
+async def backtestStrategy(symbol, year, benchmark, cash_at_risk, user_id):
+  try:
+    alpaca_api_key, alpaca_api_secret = get_alpaca_keys(user_id)
+    broker = init_broker(alpaca_api_key, alpaca_api_secret)
+    strategy = MLTrader(name="mlstrategy",
+                        broker=broker,
+                        parameters={
+                            "symbol": symbol.upper(),
+                            "cash_at_risk": cash_at_risk,
+                        })
+    start_date = datetime(year, 1, 1)
+    end_date = datetime(year, 12, 31)
+    strategy.backtest(
+        YahooDataBacktesting,
+        start_date,
+        end_date,
+        benchmark_asset=benchmark.upper(),
+        parameters={
+            "symbol": symbol.upper(),
+            "cash_at_risk": cash_at_risk,
+        },
+        show_tearsheet=False,
+        show_plot=False
+    )
+    state = "Completed"
+  except:
+     state = "Error"
+  return state
+   
+"""
+API ENDPOINTS
+"""
+
+@app.get("/api/news")
+def get_news():
+    news, status_code = fetch_crypto_news()
+    print("Reached 1")
+    if status_code == 429:
+        return JSONResponse(content={"error": "Rate limit exceeded. Please try again later."}, status_code=429)
+    processed_news = process_news(news)
+    return JSONResponse(content=processed_news)
+
+@app.post("/api/process")
+async def process_data(bp: BacktestParameters):
+    try:
+        backtest_status["status"] = "running"
+        result = await backtestStrategy(bp.symbol, int(bp.year), bp.benchmark, float(bp.cashAtRisk), bp.userId)
+        backtest_status["status"] = "complete"
+        cleanup_logs_files()
+        return {"result": result}
+    except Exception as e:
+        backtest_status["status"] = "error"
+        print(f"Error in process_data: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/api/status")
+async def get_status():
+    return backtest_status
+
 @app.get("/api/tearsheet")
 async def get_tearsheet():
     try:
         file_path = os.path.join(LOGS_DIRECTORY, 'tearsheet.html')
+        print(file_path)
         if not os.path.isfile(file_path):
             raise HTTPException(status_code=404, detail="Tearsheet not found")
         return FileResponse(file_path, media_type='text/html', filename='tearsheet.html')
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))    
