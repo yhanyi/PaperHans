2e7ff0d3ea2ffe83f39889777c87a190110859dc
yhanyi <yeohhanyi0916@gmail.com>
2024-07-29 08:32:37 +0800

    Deployment attempt

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_API_BASE_URL + "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_BASE_URL
+          ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`
+          : "http://localhost:8000/api/:path*",
         // source: "/api/:path*",
         // destination:
         //   process.env.NODE_ENV === "development"
